6ebb8c4aa5e8c210737154e6063fd8fd359efd6e
Ghost6818 <96416735+Ghost6818@users.noreply.github.com>
2023-10-27 16:19:20 +0200

    repering connection between scenes

--- Gra 2D/Assets/scripts/tunel.cs
+++ Gra 2D/Assets/scripts/tunel.cs
@@ -5,13 +5,14 @@ using UnityEngine.SceneManagement;
 
 public class tunel : MonoBehaviour
 {
+    public int mapindex = 0;
     private void OnCollisionEnter2D(Collision2D collision)
     {
         var player = collision.gameObject.GetComponent<playerContlorer>();
 
         if (player != null && player.points != false)
         {
-            SceneManager.LoadScene("SecendLevel");
+            SceneManager.LoadScene(mapindex);
         }
         Debug.Log("tunel");
     }
