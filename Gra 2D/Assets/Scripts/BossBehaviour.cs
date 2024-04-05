163dfe9cb5e98b4f6729f59073bf09142170a4c5
lemon3.000 <u51_vlaada_waw@technischools.com>
2024-04-05 15:52:53 +0200

    Boss logic

---
+++ Gra 2D/Assets/Scripts/BossBehaviour.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class BossBehaviour : MonoBehaviour
+{
+    [SerializeField] private Bullet bulletPrefab;
+    [SerializeField] private List<Transform> aim;
+    private WaitForSeconds waitTimeToShoot = new WaitForSeconds(2);
+    void SpamBullet(Transform aim)
+    {
+        Bullet bullet = Instantiate(bulletPrefab, aim.position, aim.rotation);
+    }
+    void Shoot()
+    {
+        foreach (Transform t in aim)
+        {
+            SpamBullet(t);
+        }
+        StartCoroutine(WaitToShoot());
+    }
+    IEnumerator WaitToShoot()
+    {
+        yield return waitTimeToShoot;
+        Shoot();
+    }
+    private void Awake()
+    {
+        StartCoroutine(WaitToShoot());
+    }
+
+}
